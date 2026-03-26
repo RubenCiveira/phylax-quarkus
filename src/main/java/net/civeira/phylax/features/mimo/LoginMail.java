a7edc99cc7170a886d03aa4bf1dff4a9af1b3e14
Ruben civeira <ruben.civeira@gmail.com>
2026-03-26 17:46:24 +0100

    fix: remove customizazion

--- src/main/java/net/civeira/phylax/features/mimo/LoginMail.java
+++ src/main/java/net/civeira/phylax/features/mimo/LoginMail.java
@@ -5,7 +5,6 @@ import jakarta.enterprise.event.Observes;
 import lombok.RequiredArgsConstructor;
 import net.civeira.phylax.common.infrastructure.mail.EmailMessage;
 import net.civeira.phylax.common.infrastructure.mail.EmailService;
-import net.civeira.phylax.common.infrastructure.mail.MailConfiguration;
 import net.civeira.phylax.features.oauth.authentication.domain.AuthenticationMode;
 import net.civeira.phylax.features.oauth.authentication.domain.event.LoginSucceededEvent;
 
@@ -19,13 +18,10 @@ public class LoginMail {
     System.err.println("WITH: " + login.getMode());
     if (login.getMode() == AuthenticationMode.PASSWORD
         || login.getMode() == AuthenticationMode.MFA) {
-      MailConfiguration mailConfiguration = MailConfiguration.builder().useTtls(true)
-          .smtpHost("smtp.gmail.com").smtpPort(587).fromEmail("osintdagda@gmail.com")
-          .smtpLogin("osintdagda@gmail.com").smtpPass("bcspvmufkdajrukm").build();
       EmailMessage message = EmailMessage.builder().targetAddress("rubenciveira@gmail.com")
           .targetName("Ruben").subject("Login").content("Hay un login")
           .htmlContent("<h1>Hay un login</h1><p>Un usuario hizo login</p>").build();
-      mailer.delaySendMessage(mailConfiguration, message);
+      mailer.delaySendMessage(message);
     }
   }
 }
